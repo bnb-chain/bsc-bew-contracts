04406eff40096dab2c9c8e8de04db6d3ed2e8ec3
yutianwu <wzxingbupt@gmail.com>
2021-10-19 17:42:48 +0800

    fix import sequence

--- contracts/BewSwapImplement.sol
+++ contracts/BewSwapImplement.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts-upgradeable/utils/AddressUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
-contract BewSwap is ContextUpgradeable, ReentrancyGuardUpgradeable, Initializable {
+contract BewSwap is Initializable, ContextUpgradeable, ReentrancyGuardUpgradeable {
 
     using SafeERC20Upgradeable for IERC20Upgradeable;
 
